aaf97b3d103613b3a2022f3930bf7a793ba0c056
hees <hyunsse5@gmail.com>
2025-04-18 14:56:55 +0900

    skilllist entity and dto

--- src/main/java/net/gakumas/skill/Skill.java
+++ src/main/java/net/gakumas/skill/Skill.java
@@ -1,6 +1,7 @@
 package net.gakumas.skill;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -10,5 +11,21 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 public class Skill {
+    @Schema(description="skillid")
+    Long skillId;
 
+    @Schema(description = "type 0=active, 1=mental")
+    int skillType;
+
+    @Schema(description = "ptype 0=free,")
+    int skillPType;
+
+    @Schema(description = "name in Korean")
+    String NameKR;
+
+    @Schema(description = "name in Japanese")
+    String NameJP;
+
+    @Schema(description = "rarity 0=normal,")
+    int Rarity;
 }
--- src/main/java/net/gakumas/skill/SkillApiController.java
+++ src/main/java/net/gakumas/skill/SkillApiController.java
@@ -1,11 +1,13 @@
 package net.gakumas.skill;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @AllArgsConstructor
-
+@RestController
 
 public class SkillApiController {
 }
